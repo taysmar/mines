6196103df02e4ceb7903ab654a23ec32e5d2213a
Taysmar <taysmar@outlook.com>
2021-05-25 23:28:12 -0300

    Criação do campo

--- App.js
+++ App.js
@@ -1,33 +1,47 @@
 import React, { Component } from 'react';
 import params from './src/params'
 import Field from './src/components/Field'
+import MineField from './src/components/MineField'
 import {
   StyleSheet,
   Text,
   Platform,
   View,
 } from 'react-native';
-import Flag from './src/components/Flag';
+import { createMinedBoard } from './src/functions' 
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = this.createState()
+  }
+
+  minesAmount = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return Math.ceil(cols * rows * params.difficultLevel)
+  }
+
+  createState = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return {
+      board: createMinedBoard(rows, cols, this.minesAmount()),
+    }
+
+  }
+
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Iniciando o Mines!</Text>
         <Text style={styles.welcome}>Tamanho da grade:
     {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
-
-    <Field />
-    <Field opened />
-    <Field opened nearMines={1} />
-    <Field opened nearMines={2} />
-    <Field opened nearMines={3} />
-    <Field opened nearMines={6} />
-    <Field mined />
-    <Field mined opened />
-    <Field mined opened exploded />
-    <Field flagged />
-    <Field flagged opened />
+    <View style={styles.board}>
+      <MineField board={this.state.board} />
+    </View>
     
     </View>
     );
@@ -37,10 +51,12 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
+    justifyContent: 'flex-end',
   },
+  board: {
+    alignItems: 'center',
+    backgroundColor: '#AAA'
+  }
 
 });
 
